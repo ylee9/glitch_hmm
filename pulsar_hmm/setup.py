686919983d30990edfa257a140aa6ccb41c57e69
Liam Dunn <liamd@student.unimelb.edu.au>
2021-03-25 15:58:11 +1100

    Fix name of README file in pulsar_hmm/setup.py

--- pulsar_hmm/setup.py
+++ pulsar_hmm/setup.py
@@ -5,5 +5,5 @@ setup(
     version='0.1dev',
     packages=['pulsar_hmm',],
     license='MIT License',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
 )
